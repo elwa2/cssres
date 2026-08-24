ba2dafe6fddec18fb428dd32635c0c7dec8bda2f
Ali Ahmed <115338268+elwa2@users.noreply.github.com>
2026-08-25 01:26:45 +0300

    1

--- nouvona.js
+++ nouvona.js
@@ -22,7 +22,7 @@
                     padding-right: 15px;
                     margin-left: auto;
                     margin-right: auto;
-                    max-width: 1280px;
+                    max-width: 1440px;
                     width: 100%;
                 }
                 .nd-banner-wrapper {
